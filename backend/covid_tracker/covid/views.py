fd27d9820b9d706af7324eac69a116d53e2f4db9
Hassan Sultan <hassansultan902@gmail.com>
2021-08-19 07:02:02 -0700

    Optimized Code

--- backend/covid_tracker/covid/views.py
+++ backend/covid_tracker/covid/views.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
@@ -14,14 +15,40 @@ from django.contrib.auth import get_user_model # If used custom user model
 
 from .serializers import UserSerializer
 
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import serializers
+from .serializers import  AuthTokenSerializer
+from .serializers import UserSerializer
+from rest_framework import generics, authentication, permissions
 
-class CreateUserView(CreateAPIView):
 
-    model =User.objects.all()
-    
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    model = User.objects.all()
     serializer_class = UserSerializer
 
-class MedicalDataList(generics.ListAPIView):
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user  
+          
+
+
+
+class MedicalDataList(generics.CreateAPIView):
     queryset = MedicalData.objects.all()
     serializer_class = MedicalDataSerializer
 
--- backend/covid_tracker/covid_tracker/urls.py
+++ backend/covid_tracker/covid_tracker/urls.py
@@ -20,5 +20,4 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('covid/',include('covid.urls'),name= 'covid'),
-    path('auth/',obtain_auth_token)
 ]
